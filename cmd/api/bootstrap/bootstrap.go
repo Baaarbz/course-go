a0b2ca6c6f694b4c0c07dd85e42a182719be89d3
Eduardo Barbz <eduardob907@gmail.com>
2022-01-15 15:05:19 +0100

    graceful shutdown

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -4,14 +4,17 @@ import (
 	"barbz.dev/course-go/internal/pkg/course"
 	"barbz.dev/course-go/internal/platform/server"
 	"barbz.dev/course-go/internal/platform/storage/postgres"
+	"context"
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 const (
-	host = "localhost"
-	port = 8080
+	host            = "localhost"
+	port            = 8080
+	shutdownTimeout = 10 * time.Second
 
 	dbUser = "postgres"
 	dbPass = "admin"
@@ -21,19 +24,28 @@ const (
 )
 
 func Run() error {
-	postgresURI := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
-	db, err := sql.Open("postgres", postgresURI)
+	db, err := initDatabase()
 	if err != nil {
-		return err
-	}
-	// Ping DB to check if the connection was established successfully
-	if err = db.Ping(); err != nil {
 		return err
 	}
 
 	courseRepository := postgres.NewCourseRepository(db)
 	courseService := course.NewCourseService(courseRepository)
 
-	srv := server.New(host, port, courseService)
-	return srv.Run()
+	ctx, srv := server.New(context.Background(), host, port, courseService, shutdownTimeout)
+	return srv.Run(ctx)
+}
+
+func initDatabase() (*sql.DB, error) {
+	postgresURI := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
+	db, err := sql.Open("postgres", postgresURI)
+	if err != nil {
+		return nil, err
+	}
+	// Ping DB to check if the connection was established successfully
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -4,36 +4,70 @@ import (
 	"barbz.dev/course-go/internal/pkg/course"
 	"barbz.dev/course-go/internal/platform/server/handler/courses"
 	"barbz.dev/course-go/internal/platform/server/handler/health"
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 type Server struct {
-	engine        *gin.Engine
-	httpAddr      string
+	engine          *gin.Engine
+	httpAddr        string
+	shutdownTimeout time.Duration
+	// Dependencies
 	courseService course.Service
 }
 
-func New(host string, port uint, courseService course.Service) Server {
+func New(ctx context.Context, host string, port uint, courseService course.Service, shutdownTimeout time.Duration) (context.Context, Server) {
 	srv := Server{
-		engine:   gin.New(),
-		httpAddr: fmt.Sprintf("%s:%d", host, port),
-		// Dependencies
-		courseService: courseService,
+		engine:          gin.New(),
+		httpAddr:        fmt.Sprintf("%s:%d", host, port),
+		shutdownTimeout: shutdownTimeout,
+		courseService:   courseService,
 	}
 
 	srv.registerRoutes()
-	return srv
+	return serverContext(ctx), srv
 }
 
-func (s *Server) Run() error {
-	log.Println("Server running on", s.httpAddr)
-	return s.engine.Run(s.httpAddr)
+func (s *Server) Run(ctx context.Context) error {
+	log.Println("server running on", s.httpAddr)
+
+	srv := &http.Server{
+		Addr:    s.httpAddr,
+		Handler: s.engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("server shutdown", err)
+		}
+	}()
+	<-ctx.Done()
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return srv.Shutdown(ctxShutdown)
 }
 
 func (s *Server) registerRoutes() {
 	s.engine.GET("/health", health.CheckHandler())
 	s.engine.POST("/courses", courses.CreateHandler(s.courseService))
 	s.engine.GET("/courses", courses.RetrieveAll(s.courseService))
+}
+
+func serverContext(ctx context.Context) context.Context {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		<-c
+		cancel()
+	}()
+
+	return ctx
 }
